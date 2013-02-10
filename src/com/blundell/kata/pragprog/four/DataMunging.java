69bfb75356d1f811537fd703a9fed5060c21ba54
Blundell <blundell.app.dev@gmail.com>
2013-02-10 21:18:11 +0000

    Added info to start kata 4 part 2

--- src/com/blundell/kata/pragprog/four/DataMunging.java
+++ src/com/blundell/kata/pragprog/four/DataMunging.java
@@ -8,6 +8,14 @@ package com.blundell.kata.pragprog.four;
  * Write a program to output the day number (column one) with the smallest temperature spread 
  * (the maximum temperature is the second column, the minimum the third column).
  * 
+ * Part Two: Soccer League Table
+ * 
+ * The file football.dat contains the results from the English Premier League for 2001/2. 
+ * The columns labeled ‘F’ and ‘A’ contain the total number of goals scored 
+ * for and against each team in that season (so Arsenal scored 79 goals against opponents, 
+ * and had 36 goals scored against them). 
+ * Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+ * 
  * @author Blundell
  *
  */
