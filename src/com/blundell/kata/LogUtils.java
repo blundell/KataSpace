ae982c1702434e7c1588694998a9d14c03834364
Blundell <blundell.app.dev@gmail.com>
2013-02-16 15:41:37 +0000

    Merge sort, messing with my mind

--- src/com/blundell/kata/LogUtils.java
+++ src/com/blundell/kata/LogUtils.java
@@ -2,6 +2,14 @@ package com.blundell.kata;
 
 public class LogUtils {
 
+	public static int[] createRandomIntArrayOfSize(int size) {
+		int[] output = new int[size];
+		for (int i = 0; i < size; i++) {
+			output[i] = (int) (Math.random() * 100);
+		}
+		return output;
+	}
+
 	public static void print(int[] in) {
 		System.out.print("[");
 		for (int i = 0; i < in.length; i++) {
