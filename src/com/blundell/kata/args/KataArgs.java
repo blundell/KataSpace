f947d6bd7bb7f69f8413fe0ad0cae1992deb203d
Blundell <blundell.app.dev@gmail.com>
2014-01-11 16:53:25 +0000

    init params kata

---
+++ src/com/blundell/kata/args/KataArgs.java
@@ -0,0 +1,12 @@
+package com.blundell.kata.args;
+
+/**
+ * http://codingdojo.org/cgi-bin/wiki.pl?KataArgs
+ */
+public class KataArgs {
+
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+
+}
