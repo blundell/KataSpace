605d0b51649965ccdd732a96245bc1edd54941f5
Blundell <blundell.app.dev@gmail.com>
2014-01-26 20:33:12 +0000

    LeapYears kata

---
+++ src/com/blundell/kata/leapyear/LeapYears.java
@@ -0,0 +1,28 @@
+package com.blundell.kata.leapyear;
+
+public class LeapYears {
+
+    private static final boolean LEAP_YEAR = true;
+    private static final boolean COMMON_YEAR = false;
+
+    public static boolean isLeapYear(int year) {
+        if (divisibleByFour(year) && !divisibleBy100(year)) {
+            return LEAP_YEAR;
+        } else if (divisibleByFour(year) && divisibleBy100(year) && divisibleBy400(year)) {
+            return LEAP_YEAR;
+        }
+        return COMMON_YEAR;
+    }
+
+    private static boolean divisibleByFour(int year) {
+        return year % 4 == 0;
+    }
+
+    private static boolean divisibleBy100(int year) {
+        return year % 100 == 0;
+    }
+
+    private static boolean divisibleBy400(int year) {
+        return year % 400 == 0;
+    }
+}
---
+++ test/com/blundell/kata/leapyear/LeapYearsTest.java
@@ -0,0 +1,46 @@
+package com.blundell.kata.leapyear;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LeapYearsTest {
+
+    @Test
+    public void yearNotDivisibleByFour_returnsCommonYear() {
+        int year = 1995;
+
+        boolean isLeapYear = LeapYears.isLeapYear(year);
+
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void yearDivisibleByFourButNotBy100_returnsLeapYear() {
+        int year = 1996;
+
+        boolean isLeapYear = LeapYears.isLeapYear(year);
+
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void yearDivisibleByFourAndDivisibleBy100ButNotDivisibleBy400_returnsCommonYear() {
+        int year = 1900;
+
+        boolean isLeapYear = LeapYears.isLeapYear(year);
+
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void yearDivisibleByFourAndDivisibleBy100AndDivisibleBy400_returnsLeapYear() {
+        int year = 2000;
+
+        boolean isLeapYear = LeapYears.isLeapYear(year);
+
+        assertTrue(isLeapYear);
+    }
+
+}
